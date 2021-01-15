91261cf01d55c606d2eaa8ff82da459e12d6e670
Guide <poonyapat_anusonthi@cmu.ac.th>
2021-01-15 23:30:01 +0700

    update

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,7 +1,36 @@
+import { useContext } from "react";
+import { TodosContext } from "../App";
+
 const CourseCard = (props) => {
+  const {dispatch} = useContext(TodosContext)
   return (
     <>
-      <p>This is Course card</p>
+      {
+      	props.todos.map((course)=>{
+          return (
+            <>
+            <div class= "coursecard" >
+            <tr>
+                <td>
+                	<p>Subject : {course.name}</p>
+                	<p>Grade : {course.grade}</p>
+                	<p>Credit: {course.cred}</p>
+                </td>
+                <td>
+                  <button onClick ={()=>{
+                    dispatch({
+                      type : "DELETE_TODO",
+                      payload : course.id
+                    })
+                  }}>X</button>
+                </td>
+              </tr>
+            </div>
+           </>
+          )
+        }
+       )
+      }
     </>
   );
 };
--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,12 +1,59 @@
 import { GRADES } from "../utils/grades";
 import { CREDITS } from "../utils/credits";
 import { useContext, useState } from "react";
+import { TodosContext } from "../App";
 
 const CourseForm = () => {
+  const [inputData,setInputData] = useState({name:"",grade:"",val:0,cred:0})
+  const {dispatch} = useContext(TodosContext)
   return (
-    <>
-      <p>This is course form</p>
-    </>
+    <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+
+      <form class= "form" onSubmit = {(e)=>{
+        e.preventDefault();
+        dispatch({
+          type : "ADD_TODO",
+          payload : {
+            id: Date.now(),
+            name: inputData.name,
+            grade: inputData.grade,
+            val: inputData.val,
+            cred: inputData.cred
+          }
+        }
+       )
+     }}>
+
+
+        <input class="name"  placeholder="Input Subject Name or Subject ID" onChange ={(e)=>{
+          setInputData({...inputData,name:e.currentTarget.value})
+        }}></input>
+        <select class="grade" onChange = {(e) => {
+          setInputData({...inputData,val:e.currentTarget.value,
+                        grade:e.currentTarget.options[e.currentTarget.selectedIndex].text})
+        }}>
+          <option></option>
+          {GRADES.map((obj)=>{
+            return (<option value={obj.value}>{obj.name}</option>)
+          })}
+        </select>
+        <select class="credit" onChange ={(e) => {
+          setInputData({...inputData,cred:e.currentTarget.value})
+        }}>
+          <option></option>
+        {CREDITS.map((obj)=>{
+            return (<option value={obj}>{obj}</option>)
+          })}
+        </select>
+        <button type="submit">ADD</button>
+      </form>
+    </div>
   );
 };
 
